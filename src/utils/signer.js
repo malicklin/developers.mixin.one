86933cd959e0c092833b254a5fb56a6d130dc448
Li Yuqing <im.yuqlee@gmail.com>
2019-08-08 14:33:01 +0800

    Fix sign authentication token bugs

--- src/utils/signer.js
+++ src/utils/signer.js
@@ -1,10 +1,16 @@
 import forge from 'node-forge';
 import moment from 'moment';
+import jwt from 'jsonwebtoken';
 import uuid from 'uuid/v4';
 import LittleEndian from "int64-buffer";
+import crypto from 'crypto';
+import nacl from "tweetnacl";
 
 function Signer() {
-  signAuthenticationToken: function(uid, sid, privateKey, method, uri, body) {
+}
+
+Signer.prototype = {
+  signAuthenticationToken: function(uid, sid, method, uri, body) {
     if (typeof(body) === "object") {
       body = JSON.stringify(body);
     }
@@ -15,23 +21,26 @@ function Signer() {
     let payload = {
       uid: uid,
       sid: sid,
-      iat: moment.utc().unix() ,
+      iat: moment.utc().unix(),
       exp: expire,
       jti: uuid(),
       sig: sha256.digest().toHex()
     };
-    let seed = Uint8Array.from(Buffer.from(privateKey, 'hex'));
-    let keypair = forge.pki.ed25519.generateKeyPair({seed: seed});
-    return jwt.sign(payload, keypair.privateKey, { algorithm: 'ES256'});
+    const keyPair = crypto.createECDH('secp256k1');
+    keyPair.generateKeys();
+    let pem = `-----BEGIN PRIVATE KEY-----
+${Buffer.from(`308184020100301006072a8648ce3d020106052b8104000a046d306b0201010420${keyPair.getPrivateKey('hex')}a144034200${keyPair.getPublicKey('hex')}`, 'hex').toString('base64')}
+-----END PRIVATE KEY-----`
+    return jwt.sign(payload, pem, { algorithm: 'ES256'});
   },
 
-  signEncryptedPin: function (pin, pinToken, sessionId, privateKey, iterator) {
+  signEncryptedPin: function(pin, pinToken, sessionId, privateKey, iterator) {
     const blockSize = 16;
     let Uint64LE = LittleEndian.Int64BE;
 
-    public = Uint8Array.from(new Buffer(pinToken, 'base64'));
-    private = Uint8Array.from(Buffer.from(privateKey, 'hex'));
-    key = Buffer.from(nacl.scalarMult(asa, abp)).toString('hex');
+    let pub = Uint8Array.from(new Buffer(pinToken, 'base64'));
+    let priv = Uint8Array.from(Buffer.from(privateKey, 'hex'));
+    let key = Buffer.from(nacl.scalarMult(priv, pub)).toString('hex');
     let time = new Uint64LE(moment.utc().unix());
     time = [...time.toBuffer()].reverse();
     if (iterator == undefined || iterator === "") {
@@ -49,7 +58,7 @@ function Signer() {
 
     buf = Buffer.concat([buf, new Buffer(paddingArray)]);
     let iv16  = crypto.randomBytes(16);
-    let cipher = crypto.createCipheriv('aes-256-cbc', this.hexToBytes(key), iv16);
+    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key, 'hex'), iv16);
     cipher.setAutoPadding(false);
     let encrypted_pin_buff = cipher.update(buf, 'utf-8');
     encrypted_pin_buff = Buffer.concat([iv16 , encrypted_pin_buff]);
@@ -60,9 +69,9 @@ function Signer() {
     var bytes = [];
     for (let c=0; c<hex.length; c+=2) {
       bytes.push(parseInt(hex.substr(c, 2), 16));
-    }
+    };
     return bytes;
-  }
+  },
 };
 
 export default Signer;
